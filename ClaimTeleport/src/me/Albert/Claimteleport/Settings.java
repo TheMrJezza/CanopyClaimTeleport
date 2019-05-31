b40af8c39c0baa7098813527a06ba7f5164222b8
JB <themrjezza@gmail.com>
2019-06-01 01:53:58 +1000

    ReWork Everything

--- ClaimTeleport/src/me/Albert/Claimteleport/Settings.java
+++ ClaimTeleport/src/me/Albert/Claimteleport/Settings.java
@@ -6,40 +6,68 @@ import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 public class Settings {
-	private File SettingsFile = new File(Main.getInstance().getDataFolder(), "Settings.yml");
-	private YamlConfiguration config;
-	public String teleportSuccess;
-	public String teleporting;
-	public String playermoved;
-	public String incooldown;
-	public String noPermission;
-	public String invalidClaimID;
-	public String notNumeric;
-	public String nullInput;
-	public int delay;
-	public boolean sound;
-	public String invalidplayer;
-	public Settings() {
-		reloadSettings();
-	}
-	
-	public  void reloadSettings(){
-		if (!this.SettingsFile.exists()) {
-			Main.getInstance().saveResource("Settings.yml", true);
+	private static File SettingsFile = new File(CTMain.getInstance().getDataFolder(), "Settings.yml");
+	private static YamlConfiguration config;
+	private static String teleportSuccess = "&aTeleporting...";
+	private static String teleporting;
+	private static String playermoved;
+	private static String incooldown;
+	private static String noPermission;
+	private static String invalidClaimID;
+	private static String notNumeric;
+	private static String nullInput;
+	private static int delay;
+	private static boolean sound;
+	private static String invalidplayer;
+
+	public static void reloadSettings() {
+		if (!SettingsFile.exists()) {
+			CTMain.getInstance().saveResource("Settings.yml", true);
 		}
-		
-		this.config = YamlConfiguration.loadConfiguration(this.SettingsFile);
-		this.delay =  this.config.getInt("delay");
-		this.playermoved = ChatColor.translateAlternateColorCodes('&', this.config.getString("Messages.playermoved"));
-		this.incooldown = ChatColor.translateAlternateColorCodes('&', this.config.getString("Messages.incooldown"));
-		this.teleporting = ChatColor.translateAlternateColorCodes('&', this.config.getString("Messages.Teleporting"));
-		this.teleportSuccess = ChatColor.translateAlternateColorCodes('&', this.config.getString("Messages.TeleportSuccess"));
-		this.noPermission = ChatColor.translateAlternateColorCodes('&', this.config.getString("Messages.NoPermission"));
-		this.invalidClaimID = ChatColor.translateAlternateColorCodes('&', this.config.getString("Messages.InvalidClaimID"));
-		this.notNumeric = ChatColor.translateAlternateColorCodes('&', this.config.getString("Messages.NotNumeric"));
-		this.nullInput = ChatColor.translateAlternateColorCodes('&', this.config.getString("Messages.NullInput"));
-		this.sound =  this.config.getBoolean("Sound.Enabled");
-		this.invalidplayer = ChatColor.translateAlternateColorCodes('&', this.config.getString("Messages.InvalidPlayer"));
-	
-}
+
+		teleportSuccess = getMessage("TeleportSuccess", teleportSuccess);
+		teleporting = getMessage("Teleporting", teleporting);
+
+		config = YamlConfiguration.loadConfiguration(SettingsFile);
+		delay = config.getInt("delay");
+		playermoved = ChatColor.translateAlternateColorCodes('&', config.getString("Messages.playermoved"));
+		incooldown = ChatColor.translateAlternateColorCodes('&', config.getString("Messages.incooldown"));
+		teleporting = ChatColor.translateAlternateColorCodes('&', config.getString("Messages.Teleporting"));
+		teleportSuccess = getMessage("TeleportSuccess", teleportSuccess);
+		//setNoPermission(ChatColor.translateAlternateColorCodes('&', config.getString("Messages.NoPermission")));
+		invalidClaimID = ChatColor.translateAlternateColorCodes('&', config.getString("Messages.InvalidClaimID"));
+		notNumeric = ChatColor.translateAlternateColorCodes('&', config.getString("Messages.NotNumeric"));
+		nullInput = ChatColor.translateAlternateColorCodes('&', config.getString("Messages.NullInput"));
+		sound = config.getBoolean("Sound.Enabled");
+		invalidplayer = ChatColor.translateAlternateColorCodes('&', config.getString("Messages.InvalidPlayer"));
+
+	}
+
+	private static String getMessage(String key, String defaultValue) {
+		return ChatColor.translateAlternateColorCodes('&', config.getString("Messages." + key, defaultValue));
+	}
+
+	public static boolean isSoundEnabled() {
+		return sound;
+	}
+
+	public static String invalidClaimID() {
+		return invalidClaimID;
+	}
+
+	public static String noPermission() {
+		return noPermission;
+	}
+
+	public static String notNumeric() {
+		return notNumeric;
+	}
+
+	public static void setNotNumeric(String notNumeric) {
+		Settings.notNumeric = notNumeric;
+	}
+
+	public static int getDelay() {
+		return delay;
+	}
 }
